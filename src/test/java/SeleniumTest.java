0e3be0a247dff6e8094882905965de4e7eb92262
Amadeusz1991 <amadeusz.jankowski@capgemini.com>
2021-07-11 14:16:27 +0200

    Creating drivers for firefox and IE as well, IE is more prolematic that other browsers.

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,5 +1,8 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
@@ -7,7 +10,11 @@ public class SeleniumTest {
     @Test
     public void openGooglePage(){
         System.setProperty("webdriver.chrome.driver","C:\\Users\\ajankows\\Desktop\\ChromeDriver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\ajankows\\Desktop\\ChromeDriver\\geckodriver.exe");
+        System.setProperty("webdriver.ie.driver", "C:\\Users\\ajankows\\Desktop\\ChromeDriver\\IEDriverServer.exe");
+        InternetExplorerOptions options = new InternetExplorerOptions();
+        options.withInitialBrowserUrl("https://www.google.com/");
+        WebDriver driver = new InternetExplorerDriver(options);
+        //driver.get("https://www.google.com/");
     }
 }
