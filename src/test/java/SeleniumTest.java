349e49b0434ff69d8460a60ad547969f662a0999
Amadeusz1991 <amadeusz.jankowski@capgemini.com>
2021-07-11 14:28:06 +0200

    Created Method which helps us to select any browser we want faster. Switch loop with Exception argument

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -9,12 +10,22 @@ public class SeleniumTest {
 
     @Test
     public void openGooglePage(){
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\ajankows\\Desktop\\ChromeDriver\\chromedriver.exe");
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\ajankows\\Desktop\\ChromeDriver\\geckodriver.exe");
-        System.setProperty("webdriver.ie.driver", "C:\\Users\\ajankows\\Desktop\\ChromeDriver\\IEDriverServer.exe");
-        InternetExplorerOptions options = new InternetExplorerOptions();
-        options.withInitialBrowserUrl("https://www.google.com/");
-        WebDriver driver = new InternetExplorerDriver(options);
-        //driver.get("https://www.google.com/");
+        WebDriver driver = getDriver("chrome");
+    }
+
+    public WebDriver getDriver(String browser){
+        switch (browser){
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver","C:\\Users\\ajankows\\Desktop\\ChromeDriver\\chromedriver.exe");
+                return new ChromeDriver();
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", "C:\\Users\\ajankows\\Desktop\\ChromeDriver\\geckodriver.exe");
+                return new FirefoxDriver();
+            case "ie":
+                System.setProperty("webdriver.ie.driver", "C:\\Users\\ajankows\\Desktop\\ChromeDriver\\IEDriverServer.exe");
+                return new InternetExplorerDriver();
+            default:
+                throw new InvalidArgumentException("Invalid browser name");
+        }
     }
 }
