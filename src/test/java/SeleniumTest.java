e2ff0587e790f16d10f1cbb0cf063f6b28326a4b
Amadeusz1991 <amadeusz.jankowski@capgemini.com>
2021-07-12 13:44:27 +0200

    Using new Options, which helps us with accepting unexpected popups

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,5 +1,4 @@
-import org.openqa.selenium.InvalidArgumentException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
@@ -11,11 +10,18 @@ public class SeleniumTest {
     @Test
     public void openGooglePage(){
         WebDriver driver = getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("window.open('https://www.wp.pl', 'blank', 'height=200, width=200')");
+        driver.close();
+
     }
 
     public WebDriver getDriver(String browser){
         switch (browser){
             case "chrome":
+                System.setProperty("webdriver.chrome.driver", "C:\\Users\\ajankows\\Desktop\\ChromeDriver\\chromedriver.exe");
                 return new ChromeDriver();
             case "firefox":
                 System.setProperty("webdriver.gecko.driver", "C:\\Users\\ajankows\\Desktop\\ChromeDriver\\geckodriver.exe");
--- src/test/java/WebDriverManagerTest.java
+++ src/test/java/WebDriverManagerTest.java
@@ -1,7 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
@@ -11,9 +11,14 @@ public class WebDriverManagerTest {
     @Test
     public void openBrowser(){
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        Dimension windowSize = new Dimension(200, 200);
-        driver.manage().window().setSize(windowSize);
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(false);
+        options.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.ACCEPT);
+        WebDriver driver = new ChromeDriver(options);
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("alert('Hello')");
+        driver.get("https://www.google.com");
+
+
     }
 }
